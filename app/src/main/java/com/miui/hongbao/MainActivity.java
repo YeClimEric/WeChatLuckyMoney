3de15d72c2e9259eb987349f4870f39b4eda586c
huangdp-a <tianhuanxings@163.com>
2015-12-09 13:21:08 +0800

    Fix README. test

--- app/src/main/java/com/miui/hongbao/MainActivity.java
+++ app/src/main/java/com/miui/hongbao/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         switchPlugin = (Button) findViewById(R.id.button_accessible);
+        //aa
 
         handleMIUIStatusBar();
         updateServiceStatus();
